e1bdf373a0fab8973004e0027f9c1d8448f7c7f1
nishant <nishant15066@iiitd.ac.in>
2018-03-29 01:07:03 +0530

    Created a Linked List. Need to add a more modular reverse function.

---
+++ LinkedList.java
@@ -0,0 +1,87 @@
+import java.util.Scanner;
+
+class Node{
+    int data;
+    Node next;
+    Node(int data )
+    {
+        this.data = data;
+        this.next = null;
+    }
+}
+public class LinkedList {
+
+    public static void main( String args[] )
+    {
+        LinkedList list = new LinkedList();
+        Scanner scanner = new Scanner( System.in );
+        int n = scanner.nextInt();
+        for( int i = 0; i < n; i++ )
+        {
+            int x = scanner.nextInt();
+            list.addNode(x);
+        }
+        list.head = list.reverseList();
+        list.print();
+    }
+
+    Node head;
+
+    public LinkedList(Node head)
+    {
+        this.head = head;
+    }
+
+    public LinkedList()
+    {
+        this.head = null;
+    }
+
+    private void addNode(int data )
+    {
+        Node newNode = new Node(data);
+        if( head == null )
+        {
+            this.head = newNode;
+            return;
+        }
+
+        Node prev = null;
+        Node iter = head;
+        while( iter != null )
+        {
+            prev = iter;
+            iter = iter.next;
+        }
+        prev.next = newNode;
+    }
+
+    private void print()
+    {
+        Node iter = head;
+        while( iter != null )
+        {
+            System.out.println(iter.data);
+            iter = iter.next;
+        }
+    }
+
+    private Node reverseList()
+    {
+        Node curr, nextNode, prevNode;
+        prevNode = null;
+        curr = head;
+        nextNode = curr.next;
+        while( curr.next != null )
+        {
+            curr.next = prevNode;
+//            nextNode.next = curr;
+            prevNode = curr;
+            curr = nextNode;
+            nextNode = nextNode.next;
+        }
+        curr.next = prevNode;
+        return curr;
+    }
+
+}
